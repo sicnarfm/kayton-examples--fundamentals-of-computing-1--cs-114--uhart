a44baaf6a02b02b2854d9271e9e99550d6805325
Roy A Vanegas <roy@thecodeeducators.com>
2026-09-08 09:19:15 -0400

    Break long lines at the 85-char margin

--- MaxValuesForNumericPrimitives.java
+++ MaxValuesForNumericPrimitives.java
@@ -7,17 +7,34 @@ public class MaxValuesForNumericPrimitives {
     //
     // byte, short, integer, long, float, and double
     //
-    System.out.println("A " + CLIColors.RED + "byte" + CLIColors.NORMAL + "’s max value is " + CLIColors.RED + Byte.MAX_VALUE + CLIColors.NORMAL);
+    System.out.println("A " + CLIColors.RED + "byte" + CLIColors.NORMAL +
+      "’s max value is " + CLIColors.RED + Byte.MAX_VALUE + CLIColors.NORMAL);
+
     System.out.println();
-    System.out.println("A " + CLIColors.CYAN + "short" + CLIColors.NORMAL + "’s max value is " + CLIColors.CYAN + Short.MAX_VALUE + CLIColors.NORMAL);
+
+    System.out.println("A " + CLIColors.CYAN + "short" + CLIColors.NORMAL +
+      "’s max value is " + CLIColors.CYAN + Short.MAX_VALUE + CLIColors.NORMAL);
+
     System.out.println();
-    System.out.println("An " + CLIColors.GREEN + "int" + CLIColors.NORMAL + "’s max value is " + CLIColors.GREEN + Integer.MAX_VALUE + CLIColors.NORMAL);
+
+    System.out.println("An " + CLIColors.GREEN + "int" + CLIColors.NORMAL +
+      "’s max value is " + CLIColors.GREEN + Integer.MAX_VALUE + CLIColors.NORMAL);
+
     System.out.println();
-    System.out.println("An " + CLIColors.BLUE + "long" + CLIColors.NORMAL + "’s max value is " + CLIColors.BLUE + Integer.MAX_VALUE + CLIColors.NORMAL);
+
+    System.out.println("An " + CLIColors.BLUE + "long" + CLIColors.NORMAL +
+      "’s max value is " + CLIColors.BLUE + Integer.MAX_VALUE + CLIColors.NORMAL);
+
     System.out.println();
-    System.out.println("A " + CLIColors.YELLOW + "float" + CLIColors.NORMAL + "’s max value is " + CLIColors.YELLOW + Float.MAX_VALUE + CLIColors.NORMAL);
+
+    System.out.println("A " + CLIColors.YELLOW + "float" + CLIColors.NORMAL +
+      "’s max value is " + CLIColors.YELLOW + Float.MAX_VALUE + CLIColors.NORMAL);
+
     System.out.println();
-    System.out.println("A " + CLIColors.MAGENTA + "double" + CLIColors.NORMAL + "’s max value is " + CLIColors.MAGENTA + Double.MAX_VALUE + CLIColors.NORMAL);
+
+    System.out.println("A " + CLIColors.MAGENTA + "double" + CLIColors.NORMAL +
+      "’s max value is " + CLIColors.MAGENTA + Double.MAX_VALUE + CLIColors.NORMAL);
+
     System.out.println();
   }
 }
